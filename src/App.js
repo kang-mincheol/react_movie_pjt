832953abf6350b9cea5e40fb9de75757cc9c8f5a
mincheol <aaa6315@gmail.com>
2024-02-16 02:34:24 +0900

    axios 연습

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React, {useEffect, useState} from "react";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState(false);
+
+  const getMovies = () => {
+    fetch("https://yts.mx/api/v2/list_movies.json")
+      .then(res => res.json())
+      .then((res) => {
+        console.log(res);
+        setMovies(res);
+      })
+    // try {
+    //   const response = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    //   setMovies(response);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+  }
 
   useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  })
+    // getMovies();
+    // console.log(movies);
+    // setTimeout(() => {
+    //   setIsLoading(false);
+    // }, 3000);
+    console.log(1);
+  }, [movies]);
+
+  
   
   return (
     <div className="App">
